224112603f218c8dca1a3e4c93dc1201226e23c7
Philip Scott <pscott@zeptohost.com>
2018-01-07 20:17:07 -0800

    Add rate limiter

--- routes/enqueue.js
+++ routes/enqueue.js
@@ -1,9 +1,14 @@
 const listWebms = require('4chan-list-webm')
+const RateLimiter = require('limiter').RateLimiter
 const router = require('express').Router()
+const limiter = new RateLimiter(1, 1000)
 
 router.get('/:board/:threadNo', (req, res) => {
   const { board, threadNo } = req.params
-  listWebms(board, threadNo, res.send.bind(res))
+  
+  limiter.removeTokens(1, () => {
+    listWebms(board, threadNo, res.send.bind(res))
+  })
 })
 
 module.exports = router
