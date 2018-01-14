7f7a9bb73560bd9aaa0aeb271f0a9c44ae624efe
scootykins <scootykins@yahoo.com>
2018-01-13 19:55:48 -0800

    Enable trust proxy for handling IPs with Heroku

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const routes = require('./routes/index')
 
+app.enable('trust proxy')
 app.use(routes)
 app.use(express.static(path.join(__dirname, './static')))
 
