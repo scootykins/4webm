9d37fb47fee854676e0bec605df1f23c7e62cbec
scootykins <scootykins@yahoo.com>
2018-01-28 15:27:09 -0800

    Add temporary thumbnail file serving

--- routes/enqueue.js
+++ routes/enqueue.js
@@ -1,13 +1,42 @@
+const { promisify } = require('util')
+const { RateLimiter } = require('limiter')
+const collector = require('../util/collection')
+const fs = require('fs')
+const path = require('path')
+const download = require('download')
+const delay = require('timeout-as-promise')
 const listWebms = require('4chan-list-webm')
-const RateLimiter = require('limiter').RateLimiter
 const router = require('express').Router()
+
+const unlinkAsync = promisify(fs.unlink)
 const limiter = new RateLimiter(1, 1000)
 
 router.get('/:board/thread/:threadNo', (req, res) => {
   const { board, threadNo } = req.params
-  
+
   limiter.removeTokens(1, () => {
-    listWebms(board, threadNo, res.send.bind(res))
+    listWebms(board, threadNo, (webms) => {
+      const reg = /http:\/\/i\.4cdn\.org\/(.*)\/(.*)/g
+      const collect = collector(webms)
+      const thumbnails = collect('thumbnail')
+
+      Promise.all(thumbnails.map(x => download(x, path.join(__dirname, '../thumbnail'))))
+        .then(() => {
+          const thumbnailPaths = []
+          console.log('files downloaded!')
+          webms.forEach(elem => {
+            elem.thumbnail = elem.thumbnail.replace(reg, '/thumbnail/$2')
+            thumbnailPaths.push(path.join(__dirname, `..${elem.thumbnail}`))
+          })
+          res.send(webms)
+          
+          delay('60000').then(() => {
+            Promise.all(thumbnailPaths.map(x => unlinkAsync(x))).then(() => {
+              console.log('deleted all images')
+            })
+          })
+        })
+    })
   })
 })
 
---
+++ util/collection.js
@@ -0,0 +1,7 @@
+function collector (arr) {
+  return function collect (key) {
+    return arr.map(obj => obj[key])
+  }
+}
+
+module.exports = collector
