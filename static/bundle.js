a9b7f857aec934491faa12030becce4246dc4bcb
Philip Scott <pscott@zeptohost.com>
2018-01-28 19:27:09 -0800

    Fix 4chan to 4webm URL replace feature

--- static/bundle.js
+++ static/bundle.js
@@ -956,7 +956,7 @@ var player = new __WEBPACK_IMPORTED_MODULE_0__player__["a" /* default */]({
 })
 
 if (window.location.pathname !== '/') {
-  player.load(window.location.pathname)
+  player.load(window.location.href)
 }
 
 Object(__WEBPACK_IMPORTED_MODULE_1__util__["a" /* $ */])('#thread-form').addEventListener('submit', function (e) {
@@ -1009,7 +1009,6 @@ Player.prototype.load = function load (threadUrl) {
     .then(function (res) {
       var collect = Object(__WEBPACK_IMPORTED_MODULE_2__util__["b" /* collector */])(res.data)
       this$1._webmUrls = collect('url')
-      this$1._playlist.reset()
       this$1._playlist.gen(
         collect('filename'),
         collect('thumbnail'),
