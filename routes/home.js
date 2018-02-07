60461d6d5c7373fdb158b2cdbad0597cf5b996c2
Philip Scott <pscott@zeptohost.com>
2018-02-06 22:18:54 -0800

    Add templating engine

--- routes/home.js
+++ routes/home.js
@@ -1,29 +1,49 @@
 'use strict'
 
-const path = require('path')
 const { Router } = require('express')
 const boards = require('4chan-boards')
+const { welcomeMsg, invalidMsg } = require('../util/messages')
 
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/index.html'))
+  res.render('index', {
+    global_style: '/yotsuba.css',
+    style: '/style.css',
+    messages: [welcomeMsg]
+  })
 })
 
 router.get('/:board/thread/:threadNo', (req, res) => {
   const { board } = req.params
+  let locals
+
   switch (boards.getType(board)) {
     case boards.NSFW:
-      res.sendFile(path.join(__dirname, '../views/index.html'))
+      locals = { 
+        global_style: '/yotsuba.css',
+        style: '/style.css',
+        messages: [welcomeMsg]
+      }
       break
     case boards.ADMIN:
     case boards.SFW:
-      res.sendFile(path.join(__dirname, '../views/sfw.html'))
+      locals = {
+        global_style: '/yotsuba-blue.css',
+        style: '/style-blue.css',
+        messages: [welcomeMsg]
+      }
       break
     default:
-      res.sendFile(path.join(__dirname, '../views/invalid.html'))
+      locals = {
+        global_style: '/yotsuba.css',
+        style: '/style.css',
+        messages: [invalidMsg, welcomeMsg]
+      }
       break
   }
+
+  res.render('index', locals)
 })
 
 module.exports = router
