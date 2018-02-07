52d1c4d8b9bc8597605184e2541f0798beab5623
Philip Scott <pscott@zeptohost.com>
2018-02-06 22:46:54 -0800

    Fix yotsuba-blue favicon

--- routes/home.js
+++ routes/home.js
@@ -5,13 +5,21 @@ const boards = require('4chan-boards')
 const { welcomeMsg, invalidMsg } = require('../util/messages')
 
 const router = Router()
+const defaultLocals = {
+  global_style: '/yotsuba.css',
+  style: '/style.css',
+  favicon: '/favicon.ico',
+  messages: [welcomeMsg]
+}
+const blueLocals = {
+  global_style: '/yotsuba-blue.css',
+  style: '/style-blue.css',
+  favicon: '/favicon-blue.ico',
+  messages: [welcomeMsg]
+}
 
 router.get('/', (req, res) => {
-  res.render('index', {
-    global_style: '/yotsuba.css',
-    style: '/style.css',
-    messages: [welcomeMsg]
-  })
+  res.render('index', defaultLocals)
 })
 
 router.get('/:board/thread/:threadNo', (req, res) => {
@@ -20,26 +28,16 @@ router.get('/:board/thread/:threadNo', (req, res) => {
 
   switch (boards.getType(board)) {
     case boards.NSFW:
-      locals = { 
-        global_style: '/yotsuba.css',
-        style: '/style.css',
-        messages: [welcomeMsg]
-      }
+      locals = defaultLocals
       break
     case boards.ADMIN:
     case boards.SFW:
-      locals = {
-        global_style: '/yotsuba-blue.css',
-        style: '/style-blue.css',
-        messages: [welcomeMsg]
-      }
+      locals = blueLocals
       break
     default:
-      locals = {
-        global_style: '/yotsuba.css',
-        style: '/style.css',
+      locals = Object.assign({}, defaultLocals, {
         messages: [invalidMsg, welcomeMsg]
-      }
+      })
       break
   }
 
