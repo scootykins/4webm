991f9dd972c45008934aacf14615134922959ee1
Philip Scott <pscott@zeptohost.com>
2018-04-15 11:20:30 -0700

    feat(redirect): Redirect users to HTTPS (#4)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const path = require('path')
 const express = require('express')
 const routes = require('./routes/index')
+const enforce = require('express-sslify')
 
 const app = express()
 
@@ -10,5 +11,9 @@ app.enable('trust proxy')
 app.set('view engine', 'hbs')
 app.use(routes)
 app.use(express.static(path.join(__dirname, './static')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }))
+}
 
 module.exports = app
