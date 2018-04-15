6f1dca1f0f9e59505f63a55fcea268b29f50d3ff
Philip Scott <pscott@zeptohost.com>
2018-04-15 11:41:13 -0700

    fix(redirect): Use redirect before registering routes (#5)

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const enforce = require('express-sslify')
 
 const app = express()
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }))
+}
+
 app.enable('trust proxy')
 app.set('view engine', 'hbs')
 app.use(routes)
 app.use(express.static(path.join(__dirname, './static')))
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(enforce.HTTPS({ trustProtoHeader: true }))
-}
 
 module.exports = app
