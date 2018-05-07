18e625c3bf034ef9149c0687b19b4796fbf84a66
scootykins <scootykins@yahoo.com>
2018-05-07 00:07:36 -0400

    feat: Add basic keyboard controls (#24)

--- util/messages.js
+++ util/messages.js
@@ -1,9 +1,11 @@
-const welcomeMsg = `
-You can also load a thread by replacing '4chan' with '4webm' in a thread URL and visiting it
-`
+const bannerMsgs = [
+  `
+You can also load a thread by replacing '4chan' with '4webm' in a thread URL and visiting it`,
+  `Left/right arrow keys now skip to prev/next video, and spacebar toggles pause/play`
+]
 
 const invalidMsg = `
 INVALID BOARD SHORT NAME IN THREAD URL
 `
 
-module.exports = { welcomeMsg, invalidMsg }
+module.exports = { bannerMsgs, invalidMsg }
