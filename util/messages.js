ff84b5ecfc6a747dd7e983a31ae9e2f55045c58f
Philip Scott <pscott@zeptohost.com>
2018-11-24 18:33:32 -0500

    chore: Add WIP fix message (#49)

--- util/messages.js
+++ util/messages.js
@@ -1,6 +1,6 @@
 const bannerMsgs = [
-  `
-You can also load a thread by replacing '4chan' with '4webm' in a thread URL and visiting it`,
+  '** Right now it seems 4chan sends error 403s when making a cross-origin request; I\'m working on a fix **',
+  `You can also load a thread by replacing '4chan' with '4webm' in a thread URL and visiting it`,
   `Keyboard shortcuts: Left/right -> prev/next, up/down -> volume, spacebar -> pause/play, l -> loop, f -> fullscreen`
 ]
 
